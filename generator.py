c909b9277e27e62f68a29ba8b7fda16df1abb4da
Ivy Rose Fernsby <ivy.fernsby@gmail.com>
2023-04-24 14:19:27 -0500

    added hobby calculator

--- generator.py
+++ generator.py
@@ -17,7 +17,7 @@ def input_zodiac():
         sim_zodiac = zodiac_input.title()
         if sim_zodiac not in zodiac_list:
             print("That's not a Zodiac sign I'm familiar with. Let's try again!")
-    print("Your Sim's Zodiac sign as been set to: {sign}.".format(sign=sim_zodiac))
+    print("Your Sim's Zodiac sign has been set to: {sign}.".format(sign=sim_zodiac))
     return sim_zodiac
     
     
@@ -31,11 +31,25 @@ def input_personality():
                 print("That score seems to be out of range, let's ask that one again.")
         personality[key] = score
         score = -1
-    print("Your Sim's personality points have been set to:")
-    print(personality)
+    print("Thank you! Let's move on.")
     return personality
     
-    
+
+def input_interests():
+    score = -1
+    print("Last part, I need your Sim's interest points.")
+    print("There are 18 total interests, so this may be tedious. Hang in there!")
+    for key in interests:
+        while (score < 0) or (score > 10):
+            score = int(input("What is your Sim's {key} score? ".format(key=key)))
+            if (score < 0) or (score > 10):
+                print("That score seems to be out of range, let's ask that one again.")
+        interests[key] = score
+        score = -1
+    print("Very good!")
+    return interests
+
+
 def zodiac_to_asp(sim_zodiac):
     if sim_zodiac == 'Aries' or 'Taurus' or 'Cancer' or 'Sagittarius' or 'Aquarius' or 'Pisces':
         aspirations['Family'] += 1
@@ -86,19 +100,147 @@ def personality_to_asp(sim_personality):
     return aspirations
 
 
-def aspiration_chooser(sim_zodiac, sim_personality):
+def interests_to_asp(sim_interests):
+    if sim_interests['Politics'] > 6:
+        aspirations['Fortune'] += 1
+        aspirations['Popularity'] += 1
+    if sim_interests['Food'] > 6:
+        aspirations['Family'] += 1
+        aspirations['Pleasure'] += 1
+    if sim_interests['Crime'] > 6:
+        aspirations['Knowledge'] += 1
+        aspirations['Fortune'] += 1
+    if sim_interests['Sports'] < 4:
+        aspirations['Knowledge'] += 1
+    if sim_interests['Work'] > 6:
+        aspirations['Fortune'] += 1
+        aspirations['Knowledge'] += 1
+    if sim_interests['School'] > 6:
+        aspirations['Family'] += 2
+        aspirations['Knowledge'] += 1
+    if sim_interests['Money'] > 6:
+        aspirations['Fortune'] += 2
+        aspirations['Romance'] += 1
+    if sim_interests['Entertainment'] > 6:
+        aspirations['Pleasure'] += 2
+    if sim_interests['Health'] > 6:
+        aspirations['Romance'] += 2
+        aspirations['Popularity'] += 1
+    if sim_interests['Paranormal'] > 6:
+        aspirations['Knowledge'] += 1
+    if sim_interests['Weather'] < 5:
+        aspirations['Pleasure'] += 1
+        aspirations['Popularity'] += 1
+        aspirations['Romance'] += 1
+    if sim_interests['Toys'] > 6:
+        aspirations['Family'] += 1
+        aspirations['Knowledge'] += 1
+    if sim_interests['Environment'] > 6:
+        aspirations['Family'] += 1
+    if sim_interests['Culture'] > 6:
+        aspirations['Fortune'] += 1
+        aspirations['Romance'] += 1
+    if sim_interests['Fashion'] > 6:
+        aspirations['Popularity'] += 1
+        aspirations['Fortune'] += 1
+    if sim_interests['Travel'] > 6:
+        aspirations['Pleasure'] += 1
+        aspirations['Popularity'] += 2
+        aspirations['Romance'] += 1
+    if sim_interests['Animals'] > 6:
+        aspirations['Family'] += 1
+        aspirations['Pleasure'] += 1
+    if sim_interests['Sci Fi'] > 6:
+        aspirations['Knowledge'] += 1
+    return aspirations
+
+
+def personality_to_hobby(sim_personality):
+    if sim_personality['Sloppy'] > 6:
+        hobbies['Nature'] += 2
+    if sim_personality['Shy'] < 5:
+        hobbies['Tinkering'] += 1
+        hobbies['Arts & Crafts'] += 3
+    if sim_personality['Shy'] > 6:
+        hobbies['Sports'] += 1
+    if sim_personality['Lazy'] < 5:
+        hobbies['Film & Literature'] += 2
+    if sim_personality['Lazy'] > 6:
+        hobbies['Sports'] += 2
+        hobbies['Fitness'] += 2
+        hobbies['Music & Dance'] += 1
+    if sim_personality['Serious'] < 5:
+        hobbies['Tinkering'] += 2
+        hobbies['Science'] += 3
+        hobbies['Fitness'] += 1
+    if sim_personality['Serious'] > 6:
+        hobbies['Film & Literature'] += 1
+        hobbies['Games'] += 3
+        hobbies['Music & Dance'] += 2
+    for key in sim_personality:
+        if (sim_personality[key] > 4) and (sim_personality[key] < 7):
+            hobbies['Cuisine'] += 1
+    return hobbies
+
+
+def interests_to_hobby(sim_interests):
+    if sim_interests['Food'] > 6:
+        hobbies['Cuisine'] += 2
+    if sim_interests['Entertainment'] > 6:
+        hobbies['Film & Literature'] += 2
+        hobbies['Games'] += 2
+        hobbies['Music & Dance'] += 2
+    if sim_interests['Culture'] > 6:
+        hobbies['Film & Literature'] += 2
+        hobbies['Arts & Crafts'] += 2
+        hobbies['Music & Dance'] += 2
+    if sim_interests['School'] > 6:
+        hobbies['Science'] += 2
+    if sim_interests['Toys'] > 6:
+        hobbies['Games'] += 2
+        hobbies['Tinkering'] += 4
+    if sim_interests['Sci Fi'] > 6:
+        hobbies['Science'] += 2
+    if sim_interests['Fashion'] > 6:
+        hobbies['Arts & Crafts'] += 2
+    if sim_interests['Sports'] > 6:
+        hobbies['Sports'] += 4
+    if sim_interests['Health'] > 6:
+        hobbies['Fitness'] += 4
+    if sim_interests['Environment'] > 6:
+        hobbies['Nature'] += 2
+    if sim_interests['Animals'] > 6:
+        hobbies['Nature'] += 2
+    if sim_interests['Weather'] > 6:
+        hobbies['Nature'] += 1
+    return hobbies
+
+
+def aspiration_chooser(sim_zodiac, sim_personality, sim_interests):
     zodiac_to_asp(sim_zodiac)
     personality_to_asp(sim_personality)
-    print("Now calculating your score... retrieving highest aspiration...")
-    asp_score = max(aspirations)
+    interests_to_asp(sim_interests)
+    print("Now calculating your Sim's aspiration... retrieving highest score...")
+    asp_score = max(aspirations, key=aspirations.get)
     print("Your Sim's highest scoring aspiration is {asp}!".format(asp=asp_score))
+    print(aspirations)
     return asp_score
+
+
+def hobby_chooser(sim_personality, sim_interests):
+    personality_to_hobby(sim_personality)
+    interests_to_hobby(sim_interests)
+    print("Now calculating your Sim's one true hobby... retrieving highest score...")
+    hobby_score = max(hobbies, key=hobbies.get)
+    print("Your Sim's highest scoring hobby is {hobby}!".format(hobby=hobby_score))
+    print(hobbies)
+    return hobby_score
 
 
 print("Welcome! Let's figure out who your Sim is going to be. :)")
 print("First, I need to know some things about your Sim.")
 zodiac_sign = input_zodiac()
 p_points = input_personality()
-# print(zodiac_sign)
-# print(p_points)
-aspiration_chooser(zodiac_sign, p_points)
+i_points = input_interests()
+aspiration_chooser(zodiac_sign, p_points, i_points)
+hobby_chooser(p_points, i_points)
